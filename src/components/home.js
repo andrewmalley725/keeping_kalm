3db99182e7f1932a78be89f978f0f361f883bc15
andyman <alley725@byu.edu>
2023-01-19 16:31:18 -0700

    init repo

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import data from '../js/data';
+
+export default function Home(){
+    return(
+        <div>
+            <p>This is the home page</p>
+        </div>
+    )
+}
---
+++ src/js/data.js
@@ -0,0 +1,11 @@
+var data = [
+    {
+        prodName : 'Salomon Sloth',
+        prodPrice : 25.99,
+        thumbnail : 'salomon_main.png',
+        amazon : 'https://www.amazon.com/Weighted-Stuffed-Animals-Heating-Unscented/dp/B09HP8QT6Z?ref_=ast_sto_dp'
+    }
+]
+
+
+export default data;
