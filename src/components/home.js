7b774801c589a66673685e098e34c4ee007d5fa1
andyman <alley725@byu.edu>
2023-01-19 17:18:34 -0700

    changes 1-19-23

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import data from '../js/data';
+import '../css/styles.css';
 
 export default function Home(){
     return(
         <div>
-            <p>This is the home page</p>
+            <div id='inventory'>
+                {
+                    data.map(i => {
+                        return(
+                            <div class='product'>
+                                <h4>{i.prodName}</h4>
+                                <img src={require(`../assets/${i.thumbnail}`)} alt={i.prodName}></img><br/>
+                                <b>${i.prodPrice}</b><br/>
+                                <a href={i.amazon}>Purchase on Amazon</a>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
