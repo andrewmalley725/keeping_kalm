08b3614d638232d0b00ee838f19ad69c9ce1e686
andyman <alley725@byu.edu>
2023-01-24 13:24:18 -0700

    fixed routing issues

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import data from '../js/data';
 import '../css/styles.css';
+import { Link } from 'react-router-dom';
 
 export default function Home(){
     return(
@@ -9,14 +10,14 @@ export default function Home(){
                 {
                     data.map(i => {
                         return(
-                            <a class="prodLink" href={`/#/info/${i.prodName.split(' ')[0].toLowerCase()}`}>
+                            <Link className="prodLink" to={`info/${i.prodName.split(' ')[0].toLowerCase()}`}>
                                 <div class='product'>
                                     <h4>{i.prodName}</h4>
                                     <img src={require(`../assets/${i.thumbnail}`)} alt={i.prodName}></img><br/>
                                     <b>${i.prodPrice}</b><br/>
                                     <a href={i.amazon}>Purchase on Amazon</a>
                                 </div>
-                            </a>
+                            </Link>
                         )
                     })
                 }
